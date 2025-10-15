f5bf20e8b3027780de8248c286b1404b69ee3646
wenlishi <3197066040@qq.com>
2025-10-15 18:06:30 +0800

    feat: Add Stack-Only solution for #20 Valid Parentheses

---
+++ 0020_ValidParentheses/Sulution_StackOnly.java
@@ -0,0 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Sulution_StackOnly {
+    public boolean isValid(String s) {
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+
+            }
+        }
+        return stack.isEmpty();
+    }
+    
+}
