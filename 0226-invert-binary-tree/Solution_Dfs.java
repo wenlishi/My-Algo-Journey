5b4ca5288d7572f7d8ad5f88cc37295e1747a6fc
wenlishi <3197066040@qq.com>
2025-10-20 21:11:29 +0800

    增加注释

--- 0226-invert-binary-tree/Solution_Dfs.java
+++ 0226-invert-binary-tree/Solution_Dfs.java
@@ -15,17 +15,22 @@ public class Solution_Dfs {
         if (root == null) {
             return null;
         }
+        // 创建栈
         Deque<TreeNode> stk = new ArrayDeque<>();
-
+        // 将根结点入栈
         stk.push(root);
+        // 当栈不为空持续循环
         while (!stk.isEmpty()) {
+            // 弹出栈顶元素
             TreeNode node = stk.pop();
+            // 如果当前结点存在左结点，入栈
             if (node.left != null) {
                 stk.push(node.left);
             }
             if (node.right != null) {
                 stk.push(node.right);
             }
+            // 交换当前结点的左右结点
             TreeNode tmp = node.left;
             node.left = node.right;
             node.right = tmp;
