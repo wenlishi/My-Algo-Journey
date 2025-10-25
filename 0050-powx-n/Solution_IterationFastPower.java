ac3e8739e7d1751e11736a9d1905599684ac84f5
wenlishi <3197066040@qq.com>
2025-10-25 11:28:33 +0800

    使用迭代快速幂的方法解决乘方问题

---
+++ 0050-powx-n/Solution_IterationFastPower.java
@@ -0,0 +1,20 @@
+public class Solution_IterationFastPower {
+    public double myPow(double x, int n) {
+        long N = n;
+        return N > 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+        
+    }
+    public double quickMul(double x, long N) {
+        double ans = 1.0;
+        // 
+        double x_contribute = x;
+        while (N > 0) {
+            if (N % 2 == 1) {
+                ans *= x_contribute;
+            }
+            N /= 2;
+            x_contribute *= x_contribute;
+        }
+        return ans;
+    }
+}
